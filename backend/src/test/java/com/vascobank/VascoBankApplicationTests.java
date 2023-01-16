c0bd78b973cdad656773b2af781d591edde0a56e
Maycon Jardim <mayconsmill@gmail.com>
2023-01-16 10:27:23 -0300

    Projeto criado

---
+++ backend/src/test/java/com/vascobank/VascoBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vascobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VascoBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

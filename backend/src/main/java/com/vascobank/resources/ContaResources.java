96c093d6c62d3271e49b6bb525cb89c96e2e4c90
Maycon Jardim <mayconsmill@gmail.com>
2023-01-17 10:39:30 -0300

    Retorna detalhes da conta por id

--- backend/src/main/java/com/vascobank/resources/ContaResources.java
+++ backend/src/main/java/com/vascobank/resources/ContaResources.java
@@ -1,14 +1,27 @@
 package com.vascobank.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vascobank.entities.Conta;
+import com.vascobank.repositories.ContaRepository;
 
 @RestController
 public class ContaResources {
 
+	@Autowired
+	private ContaRepository contaRepository;
+
 	@GetMapping("/contas")
-	public String getMinhaContaDetalhes() {
-		return "Retorna detalhes da conta vindo do Banco de dados";
+	public Conta getContaDetalhes(@RequestParam Long id) {
+		Conta contas = contaRepository.findByUserId(id);
+		if (contas != null) {
+			return contas;
+		} else {
+			return null;
+		}
 	}
 
 }

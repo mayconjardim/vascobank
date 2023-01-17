b3a5546b47983ee2131d000e63aa6269fc4db373
Maycon Jardim <mayconsmill@gmail.com>
2023-01-17 10:46:14 -0300

    Salva solicitação de contato

--- backend/src/main/java/com/vascobank/resources/ContatoResource.java
+++ backend/src/main/java/com/vascobank/resources/ContatoResource.java
@@ -1,14 +1,33 @@
 package com.vascobank.resources;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Date;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vascobank.entities.Contato;
+import com.vascobank.repositories.ContatoRepository;
 
 @RestController
 public class ContatoResource {
 
-	@GetMapping("/contatos")
-	public String saveContatos() {
-		return "Salva contato no banco de dados";
-	}
+    @Autowired
+    private ContatoRepository contatoRepository;
+
+    @PostMapping("/contato")
+    public Contato saveContatoDetalhes(@RequestBody Contato contato) {
+    	contato.setContatoId(getNumeroDeSolicitacao());
+    	contato.setDataCriacao(new Date(System.currentTimeMillis()));
+        return contatoRepository.save(contato);
+    }
+
+    public String getNumeroDeSolicitacao() {
+        Random random = new Random();
+        int ranNum = random.nextInt(999999999 - 9999) + 9999;
+        return "SR"+ranNum;
+    }
 	
 }

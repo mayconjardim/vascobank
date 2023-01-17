3d82198fb4bc4d2d14ad9464f0efd644ddd10294
Maycon Jardim <mayconsmill@gmail.com>
2023-01-17 10:48:03 -0300

    Retorna detalhes de emprestimo por id

--- backend/src/main/java/com/vascobank/resources/EmprestimoResources.java
+++ backend/src/main/java/com/vascobank/resources/EmprestimoResources.java
@@ -1,14 +1,29 @@
 package com.vascobank.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vascobank.entities.Emprestimos;
+import com.vascobank.repositories.EmprestimosRepository;
 
 @RestController
 public class EmprestimoResources {
 
-	@GetMapping("/emprestimos")
-	public String getEmprestimoDetalhes() {
-		return "Retorna emprestimos da conta vind do banco de dados";
-	}
+    @Autowired
+    private EmprestimosRepository emprestimosRepository;
+
+    @GetMapping("/emprestimos")
+    public List<Emprestimos> getDetalhesEmprestimos(@RequestParam Long id) {
+        List<Emprestimos> emp = emprestimosRepository.findByUserIdOrderBydataCriacaoDesc(id);
+        if (emp != null ) {
+            return emp;
+        }else {
+            return null;
+        }
+    }
 	
 }

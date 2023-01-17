e65713b02aa2d74834b93bc5ad68027a4b343d5d
Maycon Jardim <mayconsmill@gmail.com>
2023-01-17 10:41:41 -0300

    Retorna detalhes do saldo por Id

--- backend/src/main/java/com/vascobank/resources/SaldoResource.java
+++ backend/src/main/java/com/vascobank/resources/SaldoResource.java
@@ -1,14 +1,29 @@
 package com.vascobank.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vascobank.entities.Transacoes;
+import com.vascobank.repositories.TransacoesRepository;
 
 @RestController
 public class SaldoResource {
 
-	 @GetMapping("/saldos")
-	 public String getSaldoDetalhes() {
-		 return "Retorna saldo da conta vindo do banco de dados";
-	 }
-	
+	@Autowired
+	private TransacoesRepository transacoesRepository;
+
+	@GetMapping("/saldos")
+	public List<Transacoes> getDetalhesSaldo(@RequestParam Long id) {
+		List<Transacoes> transacoes = transacoesRepository.findByUserIdOrderByDataTransacaoDesc(id);
+		if (transacoes != null) {
+			return transacoes;
+		} else {
+			return null;
+		}
+	}
+
 }

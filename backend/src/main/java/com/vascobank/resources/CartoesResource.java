3e40330698ca986b32638b8c1d14e7fa367541c2
Maycon Jardim <mayconsmill@gmail.com>
2023-01-17 10:43:44 -0300

    Retorna detalhe dos cartões por id

--- backend/src/main/java/com/vascobank/resources/CartoesResource.java
+++ backend/src/main/java/com/vascobank/resources/CartoesResource.java
@@ -1,15 +1,30 @@
 package com.vascobank.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vascobank.entities.Cartoes;
+import com.vascobank.repositories.CartoesRepository;
 
 @RestController
 public class CartoesResource {
 	
-	@GetMapping("/cartoes")
-	public String getCartaoDetalhes() {
-		return "Retorna cartões da conta vind do banco de dados";
-	}
+    @Autowired
+    private CartoesRepository cartoesRepository;
+
+    @GetMapping("/cartoes")
+    public List<Cartoes> getDetalhesCartoes(@RequestParam Long id) {
+        List<Cartoes> cartoes = cartoesRepository.findByUserId(id);
+        if (cartoes != null ) {
+            return cartoes;
+        }else {
+            return null;
+        }
+    }
 
 	
 	
